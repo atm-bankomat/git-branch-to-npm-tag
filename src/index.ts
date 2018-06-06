b4154060bbdf7cdf7aa8c02eea32306aad4236d2
Jessica Kerr <jessitron@gmail.com>
2018-06-06 12:43:40 -0500

    implement

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export function gitBranchToNpmTag(branchName: string) {
+    const safeName = branchName.replace(/\//g, ":");
+    return "branch:" + safeName;
+}
---
+++ test/test.ts
@@ -0,0 +1,17 @@
+import { gitBranchToNpmTag } from "../src";
+import * as assert from "assert";
+
+describe("git branch to npm tag", () => {
+
+    it("prefixes it with branch:", () => {
+        const input = "hello-branch";
+        const result = gitBranchToNpmTag(input)
+        assert.equal(result, "branch:" + input)
+    })
+
+    it("replaces slash with colon", () => {
+        const input = "hello/branch";
+        const result = gitBranchToNpmTag(input);
+        assert(!result.includes("/"));
+    })
+})
